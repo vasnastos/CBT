f85374bf10c3ecf853e596405af5333cf81be003
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2022-05-20 18:36:28 +0300

    Add files via upload

---
+++ code_cpp/code_cpp/core.hpp
@@ -0,0 +1,112 @@
+#include "department.hpp"
+#include <filesystem>
+#include <fstream>
+#include <set>
+#include <map>
+#include <sstream>
+#include <vector>
+
+// Structure of first nine lines
+// Name: Ing0809-1
+// Courses: 142
+// Rooms: 21
+// Days: 5
+// Periods_per_day: 5
+// Curricula: 83
+// Min_Max_Daily_Lectures: 2 3
+// UnavailabilityConstraints: 841
+// RoomConstraints: 426
+
+namespace fs=std::filesystem;
+
+class Graph
+{
+    private:
+        std::map <std::string,std::vector <std::string>> adj_list;
+    public:
+        Graph();
+        void purge();
+        std::vector <std::pair <std::string,std::string>> edges;
+        std::vector <std::string> nodes;
+        void add_node(const std::string &node);
+        void add_edge(const std::string &e1,const std::string &e2);
+        bool has_edge(const std::string &e1,const std::string &e2);
+        int get_degree(const std::string &v);
+        std::vector <std::string> get_neighbors(const std::string &v);
+        friend std::ostream &operator<<(std::ostream &os,const Graph &g);
+};
+
+
+class Problem
+{
+    private:
+        std::string id;
+        std::set <std::string> lecturers;
+        std::vector <Course> courses;
+        std::vector <Room> rooms;
+        std::vector <Curricula> curriculas;
+        std::vector <std::tuple<std::string,int,int>> unavailability_constraints;
+        std::vector <std::pair <std::string,std::string>> room_constraints;
+        std::vector <Lecture> lectures;
+
+    public:
+        Graph G;
+        static std::vector <std::string> dataset_source;
+        static std::vector <std::string> dataset_names;
+        static void init_source();
+        static std::string path_to_datasets;
+        Problem(const std::string &dataset_name);
+        
+         // Abbreviations
+        int C;// number of courses
+        int R;// number of rooms
+        int CL;//number of curriculas
+        int S;// number of students;
+        int D;// total days
+        int L;// number of lectures
+        int PPD; // periods per day
+        int MNDL;// Min daily lectures
+        int MXDL;//Max Daily lectures
+        int UNC; //number of unavailability constraints
+        int RC;// number of room constraints
+        int LCS; //number of lectures
+        int P; // number of periods
+        
+        void load_udine();
+        void create_graph();
+        void clear();
+
+        double conflict_density(std::string per="course");
+        double teachers_availability(std::string per="course");
+        double room_suitability(std::string per="course");
+        double room_occupation(std::string per="room");
+        std::string lectures_per_day_per_curriculum();
+
+        int total_seats();
+        int total_students();
+        int min_curriculum_lectures();
+        int max_curriculum_lectures();
+
+        void summary();
+};
+
+class Solution
+{
+    private:
+        std::map <std::string,int> speriods;
+        std::map <int,std::pair <int,int>> speriods_format;
+        std::map <std::string,int> srooms;
+        Problem *p;
+        int cost;
+    public:
+        
+        Solution(const std::string &ds_name);
+
+        ~Solution();
+
+        int compute_cost();
+        int compute_normalize_cost();
+        bool feasibility();
+
+        void saturation_largest_first();
+};
